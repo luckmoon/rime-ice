268c5bfcdbeaa52695abddf058ca6dd622f4eb93
luckmoon <hzhcheng92@gmail.com>
2024-01-26 17:24:34 +0800

    add download sogou

---
+++ others/script/go.sh
@@ -0,0 +1,13 @@
+set -x
+set -e pipefail
+
+#source activate py311
+#eval "$(conda shell.bash hook)"
+#conda activate py311
+source ~/miniconda3/bin/activate py311
+
+cd python
+python download_sogou.py
+python scel2txt.py
+cd ../cpp
+bash go.sh
